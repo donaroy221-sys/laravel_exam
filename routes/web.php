2a6ae9f4080eb4e0a8ec876b1fbb7e66bd95053e
Dona Roy <donaroy221@gmail.com>
2025-12-15 14:06:29 +0530

    first commit

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,13 @@ use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SalaryReportController;
 use App\Http\Middleware\ValidUser;
-use App\Http\Middleware\ValidAdmin;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::middleware(ValidAdmin::class)->group(function(){
-Route::get('/register',[AuthController::class,'register'])->name('register') ;
+
+Route::get('/register',[AuthController::class,'register'])->name('register');
 
 Route::post('/register',[AuthController::class,'registerPost'])->name('register.post');
 
@@ -19,7 +19,6 @@ Route::get('/logout',[AuthController::class,'logout'])->name('logout');
 Route::get('/login',[AuthController::class,'login'])->name('login');
 
 Route::post('/login',[AuthController::class,'loginPost'])->name('login.post');
-});
 
 
 Route::resource('employee',EmployeeController::class)->middleware('auth');
